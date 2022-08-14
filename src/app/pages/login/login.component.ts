f7a6cb297f74815afce4bfb2cb8f8ad2f9136302
pedro.carvalho <pvocufc@gmail.com>
2022-08-14 16:45:56 -0300

    setting up login form strucure

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  eyeIcon: string = "../../../assets/eye-closed.svg"
-  constructor() { }
+  shoulDisplayPassword: boolean = false;
+  eyeIconClosed: string = "../../../assets/eye-closed.svg"
+  eyeIconOpen: string = "../../../assets/eye-open.svg"
+  loginForm: FormGroup = this.formBuilder.group({
+    email: '',
+    password: ''
+  })
+  
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  setEyeIcon(){
+    this.shoulDisplayPassword = !this.shoulDisplayPassword
+  }
+
+  getEyeIcon(){
+    return this.shoulDisplayPassword ? this.eyeIconOpen : this.eyeIconClosed
+  }
+
+  onSubmit(){
+    console.log('teste')
+    return true
   }
 
 }
